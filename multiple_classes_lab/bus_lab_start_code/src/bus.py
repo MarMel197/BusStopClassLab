2e441a9b9991573852e2c3ea64586708567e1b33
StuuGraham <stuartgraham7@hotmail.co.uk>
2020-10-20 16:10:22 +0100

    One big commit cos I forgot to do this

---
+++ multiple_classes_lab/bus_lab_start_code/src/bus.py
@@ -0,0 +1,30 @@
+class Bus:
+    def __init__(self, input_route_number, 
+    input_destination):
+        self.route_number = input_route_number
+        self.destination = input_destination
+        self.passengers = []
+
+    def drive(self):
+        return "Brum brum"
+
+    def passenger_count(self):
+        return len(self.passengers)
+
+    def pick_up(self, person):
+        self.passengers.append(person)
+
+    def drop_off(self, person):
+        self.passengers.remove(person)
+
+    # def empty(self, passengers):
+    #     for people in passengers
+    #         self.passengers.remove(people)
+
+    def empty(self):
+        self.passengers = []
+
+    def pick_up_from_stop(self, bus_stop):
+        for passenger in bus_stop.queue:
+            if self.destination == passenger.destination:
+                self.passengers.append(passenger)
